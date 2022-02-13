ebb67806773b1d379744e55c224e9b6e310ce5cb
Waterpicker <waterpickerenternity@gmail.com>
2022-02-13 07:31:16 -0600

    Got the basic concept workin.

--- common/src/main/java/net/examplemod/ExampleMod.java
+++ common/src/main/java/net/examplemod/ExampleMod.java
@@ -1,33 +1,56 @@
 package net.examplemod;
 
-import com.google.common.base.Suppliers;
-import dev.architectury.registry.CreativeTabRegistry;
-import dev.architectury.registry.registries.DeferredRegister;
-import dev.architectury.registry.registries.Registries;
-import dev.architectury.registry.registries.RegistrySupplier;
-import net.minecraft.core.Registry;
+import dev.architectury.event.events.client.ClientTickEvent;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 
-import java.util.function.Supplier;
+import java.util.List;
+import java.util.Set;
+
+import static dev.architectury.event.events.client.ClientTickEvent.CLIENT_POST;
+import static dev.architectury.registry.client.keymappings.KeyMappingRegistry.*;
 
 public class ExampleMod {
     public static final String MOD_ID = "examplemod";
-    // We can use this if we don't want to use DeferredRegister
-    public static final Supplier<Registries> REGISTRIES = Suppliers.memoize(() -> Registries.get(MOD_ID));
-    // Registering a new creative tab
-    public static final CreativeModeTab EXAMPLE_TAB = CreativeTabRegistry.create(new ResourceLocation(MOD_ID, "example_tab"), () ->
-            new ItemStack(ExampleMod.EXAMPLE_ITEM.get()));
-    
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registry.ITEM_REGISTRY);
-    public static final RegistrySupplier<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () ->
-            new Item(new Item.Properties().tab(ExampleMod.EXAMPLE_TAB)));
-    
-    public static void init() {
-        ITEMS.register();
-        
-        System.out.println(ExampleExpectPlatform.getConfigDirectory().toAbsolutePath().normalize().toString());
+    private static KeyMapping binding;
+
+    public static void clientInit() {
+        register(binding = new KeyMapping("key.fontswitcher", 76, "key.categories.misc"));
+        CLIENT_POST.register(new ClientTickEvent.Client() {
+            @Override
+            public void tick(Minecraft instance) {
+                if(instance.screen == null) {
+                    if(binding.isDown()) {
+                        instance.setScreen(new Screen(new TextComponent("Font Switcher")) {
+                            private int index = 0;
+
+                            @Override
+                            protected void init() {
+                                super.init();
+
+                                addRenderableWidget(new Button(0, 0, 100,20, new TextComponent("Toggle"), a -> {
+                                    List<ResourceLocation> locations = minecraft.fontManager.fontSets.keySet().stream().sorted().toList();
+
+                                    ResourceLocation location = locations.get(index = (index + 1) % locations.size());
+
+                                    Minecraft.DEFAULT_FONT = location;
+                                    Style.DEFAULT_FONT = location;
+                                }));
+
+                                addRenderableWidget(new Button(0, 20, 100,20, new TextComponent("Reset").setStyle(Style.EMPTY.withFont(new ResourceLocation("minecraft", "default"))), a -> {
+                                    Minecraft.DEFAULT_FONT = new ResourceLocation("minecraft", "default");
+                                    Style.DEFAULT_FONT = new ResourceLocation("minecraft", "default");
+                                }));
+                            }
+                        });
+                    }
+                }
+            }
+        });
     }
 }
--- fabric/src/main/java/net/examplemod/fabric/ExampleModFabric.java
+++ fabric/src/main/java/net/examplemod/fabric/ExampleModFabric.java
@@ -6,6 +6,6 @@ import net.fabricmc.api.ModInitializer;
 public class ExampleModFabric implements ModInitializer {
     @Override
     public void onInitialize() {
-        ExampleMod.init();
+        ExampleMod.clientInit();
     }
 }
